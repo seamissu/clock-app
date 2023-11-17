aff4210223cf87abebc99e756ac35d6a4d3d0114
Jingning <seamissu@gmail.com>
2023-11-17 12:03:35 +1100

    Fix Button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,10 @@
+'use client';
+
 import React from 'react';
 import styles from './Button.module.scss';
 
 function Button() {
-  return <button className={styles.button}></button>;
+  return <button className={styles.wrapper} />;
 }
 
 export default Button;
